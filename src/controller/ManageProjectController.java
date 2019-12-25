db0a8ceaab3c68a13c77225681e21891a856f8d7
Xavier La Rosa <xavier.a.larosa@gmail.com>
2019-12-25 16:19:22 -0500

    Refactored ManageProjectController.java and took out sort feature entirely.

--- src/controller/ManageProjectController.java
+++ src/controller/ManageProjectController.java
@@ -8,61 +8,53 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
+
 import model.Member;
 import model.Project;
 import model.Task;
 
-import javax.swing.*;
-import java.lang.reflect.Array;
 import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class ManageProjectController extends BasicController {
+    //Data
+    private Project project = null;
+    private Member member = null;
+
+    // Display UI
+    @FXML Text projectText, teamText;
+
+    // Button features
     @FXML Button renameProjectButton, renameTeamButton, membersButton,
             viewAnalyticsButton, updateButton, clearButton, logoutButton, homeButton;
+
+    // Search tasks
     @FXML TextField searchTextField;
-    @FXML Text projectText, teamText;
+
+    // Filter tasks
+    private List<CheckBox> boxesChecked = new ArrayList<>();
     @FXML DatePicker startDatePicker, endDatePicker;
     @FXML CheckBox statusToDoCheckBox, statusInProgressCheckBox, statusHaltedCheckBox,
             statusDoneCheckBox, diffEasyCheckBox, diffMediumCheckBox, diffHardCheckBox,
             diffUnknownCheckBox;
-    @FXML ComboBox<String> typeComboBox, directionComboBox;
-    ObservableList<String> typeComboBoxObservableList, directionComboBoxObservableList;
-    ObservableList<Task> tasksObservableList;
+
+    // TableView
+    private List<Task> filteredList = null;
+    private ObservableList<Task> tasksObservableList;
     @FXML TableView<Task> tasksTableView;
     @FXML TableColumn<Task, Date> startCol, dueCol;
-    @FXML TableColumn<Task, String> statusCol, difficultyCol, assignedCol, titleCol,
-            descriptionCol;
+    @FXML TableColumn<Task, String> statusCol, difficultyCol, assignedCol, titleCol, descriptionCol;
     @FXML TableColumn<Task, Integer> totalCommentsCol;
-    private Project project = null;
-    private Member member = null;
-    List<Task> filteredList = null;
 
     void start(Project project, Member member){
+        // set data
         this.project = project;
         this.member = member;
         printCurrentState();
         refreshScreen();
 
-        searchTextField.setOnKeyTyped((event) ->{
-            try{
-                System.out.println("detected key press");
-                if(searchTextField.getText().isEmpty()){
-                    if(filterExists()){
-                        displayFilteredTasks();
-                    } else{
-                        displayOriginalTasks();
-                    }
-                    // TODO: create a method that checks if any filter exists, if not use above code, if so, save observable list
-                } else{
-                    searchTasks(searchTextField.getText());
-                }
-            } catch(Exception e){
-                e.printStackTrace();
-            }
-        });
-
+        // add functions to checkboxes
         addActionToCheckBox(statusToDoCheckBox);
         addActionToCheckBox(statusInProgressCheckBox);
         addActionToCheckBox(statusHaltedCheckBox);
@@ -72,18 +64,7 @@ public class ManageProjectController extends BasicController {
         addActionToCheckBox(diffHardCheckBox);
         addActionToCheckBox(diffUnknownCheckBox);
 
-        List<String> types = new ArrayList<>();
-        types.add("Total Comments");
-        types.add("Alphabetical Task Title");
-        types.add("Start Date");
-        typeComboBoxObservableList = FXCollections.observableList(types);
-        typeComboBox.setItems(typeComboBoxObservableList);
-        List<String> directions = new ArrayList<>();
-        directions.add("Ascending");
-        directions.add("Descending");
-        directionComboBoxObservableList = FXCollections.observableList(directions);
-        directionComboBox.setItems(directionComboBoxObservableList);
-
+        // create TableView
         startCol.setCellValueFactory(new PropertyValueFactory<>("startDate"));
         dueCol.setCellValueFactory(new PropertyValueFactory<>("dueDate"));
         statusCol.setCellValueFactory(new PropertyValueFactory<>("status"));
@@ -93,121 +74,39 @@ public class ManageProjectController extends BasicController {
         descriptionCol.setCellValueFactory(new PropertyValueFactory<>("description"));
         totalCommentsCol.setCellValueFactory(new PropertyValueFactory<>("totalComments"));
         tasksTableView.getColumns().setAll(startCol, dueCol, statusCol, difficultyCol, assignedCol, titleCol, descriptionCol, totalCommentsCol);
-        displayOriginalTasks();
+        displayTasks(project.getTeam().getTasks());
 
-        /*
-        * Cosnidering taking out sorting feature because tableview already does it for us
-        * */
-        typeComboBox.setVisible(false);
-        directionComboBox.setVisible(false);
-    }
+        // give search bar function
+        searchTextField.setOnKeyTyped((event) ->{
+            try{
+                System.out.println("detected key press");
+                if(searchTextField.getText().isEmpty()){ // empty search field
+                    if(filterExists()){
+                        displayTasks(filteredList);
+                    } else{
+                        displayTasks(project.getTeam().getTasks());
+                    }
+                } else{ // not empty, check tasks for subset
+                    List<Task> filterList = tasksObservableList.stream().
+                            filter(s -> s.getTitle().toUpperCase().
+                                    contains(searchTextField.getText().toUpperCase()) || (s.getDescription() != null && s.getDescription().toUpperCase().
+                                    contains(searchTextField.getText().toUpperCase()))).
+                            collect(Collectors.toList());
 
-    private boolean filterExists(){
-        List<CheckBox> checkBoxes = new ArrayList<>();
-        checkBoxes.add(statusToDoCheckBox);
-        checkBoxes.add(statusInProgressCheckBox);
-        checkBoxes.add(statusHaltedCheckBox);
-        checkBoxes.add(statusDoneCheckBox);
-        checkBoxes.add(diffEasyCheckBox);
-        checkBoxes.add(diffMediumCheckBox);
-        checkBoxes.add(diffHardCheckBox);
-        checkBoxes.add(diffUnknownCheckBox);
-        for(CheckBox ch : checkBoxes){
-            if(ch.isSelected()){
-                return true;
+                    displayTasks(filterList);
+                }
+            } catch(Exception e){
+                e.printStackTrace();
             }
-        }
-
-        List<DatePicker> datePickers = new ArrayList<>();
-        datePickers.add(startDatePicker);
-        datePickers.add(endDatePicker);
-        for(DatePicker da : datePickers){
-            if(da.getValue() != null){
-                return true;
-            }
-        }
-
-        return false;
+        });
     }
 
-    private void searchTasks(String keyword){
-       List<Task> filterList = tasksObservableList.stream().
-                filter(s -> s.getTitle().toUpperCase().//convert to uppercase for checking
-                        contains(keyword.toUpperCase()) || (s.getDescription() != null && s.getDescription().toUpperCase().
-                        contains(keyword.toUpperCase()))).//filter values containing black
-                collect(Collectors.toList());//collect as list
-        tasksTableView.setItems(FXCollections.observableList(filterList));
-    }
-    private void displayOriginalTasks(){
-        tasksObservableList = FXCollections.observableList(project.getTeam().getTasks());
-        tasksTableView.setItems(tasksObservableList);
-    }
-    private void displayFilteredTasks(){
-        tasksObservableList = FXCollections.observableList(filteredList);
-        tasksTableView.setItems(tasksObservableList);
-    }
-    void addActionToCheckBox(CheckBox ch){
-        ch.setOnAction(i->updateFilter(ch));
-    }
-
-    List<CheckBox> boxesChecked = new ArrayList<>();
-    void updateFilter(CheckBox ch) {
-        System.out.println("method called");
-        if(ch.isSelected()) {
-            // if does not exist in checked list, add to checked list
-            if(!boxesChecked.contains(ch)){
-                boxesChecked.add(ch);
-            }
-        } else {
-            // if exists in checked list, delete
-            if(boxesChecked.contains(ch)){
-                boxesChecked.remove(ch);
-            }
-        }
-        System.out.println("New List of Checked Boxes: "+boxesChecked);
-    }
-
-    private void printCurrentState(){
-        System.out.println("Printing Current State of ManageProjectController: ");
-        System.out.println("\tCurrent Member: "+member.getName());
-        System.out.println("\tProject Name: "+project.getTitle());
-        System.out.println("\tTeam name: "+project.getTeam().getTitle());
-        for(Member m : project.getTeam().getMembers()){
-            System.out.println("\tMember: "+m.getName());
-        }
-        for(Task t : project.getTeam().getTasks()){
-            System.out.println("\tTask: "+t.toString());
-        }
-    }
-    private void refreshScreen(){
-        this.projectText.setText(project.getTitle());
-        this.teamText.setText(project.getTeam().getTitle());
-    }
-    public void handleRenameProjectButton(ActionEvent event){
-        System.out.println("Rename Project Tapped");
-    }
-    public void handleRenameTeamButton(ActionEvent event){
-        System.out.println("Rename Team Tapped");
-    }
-    public void handleMembersButton(ActionEvent event){
-        System.out.println("Members Tapped");
-    }
-    public void handleViewAnalyticsButton(ActionEvent event){
-        System.out.println("View Analytics Tapped");
-    }
-    public void handleUpdateButton(ActionEvent event){
+    public void handleUpdateButton(ActionEvent event){ // update table view with filtered list
         System.out.println("Update Tapped");
-        ObservableList<Task> originalTasks = FXCollections.observableArrayList(project.getTeam().getTasks());
-        FilteredList<Task> tasks = new FilteredList<>(originalTasks);
-        tasks.setPredicate(null); //used to reset filter
-        //tableView.setItems(tasks);
 
         filteredList = new ArrayList<>();
-        if(boxesChecked.size()!=0 || startDatePicker.getValue() != null
-                || endDatePicker.getValue() != null
-                || (typeComboBox.getValue() != null && directionComboBox.getValue() != null)){
-            if(boxesChecked.size()!=0){
-                System.out.println("Filtering with checkboxes");
+        if(filterExists()){ // if filter exists
+            if(!boxesChecked.isEmpty()){ // filter through checkboxes
                 List<Predicate<Task>> filtersCollection = new ArrayList<>();
                 for(CheckBox ch : boxesChecked){
                     filtersCollection.add(i -> i.getStatus().equalsIgnoreCase(ch.getText()) || i.getDifficulty().equalsIgnoreCase(ch.getText()));
@@ -219,8 +118,7 @@ public class ManageProjectController extends BasicController {
                         .forEach(filteredList::add);
                 System.out.println("Semi Filtered List: "+filteredList.toString());
             }
-            if(startDatePicker.getValue() != null){
-                System.out.println("Filtering with start date: ");
+            if(startDatePicker.getValue() != null){ // filter through start date
                 if(filteredList.size() == 0 && boxesChecked.size() == 0){
                     System.out.println("\tNo checkbox filters were used, initializing filterList for start");
                     filteredList.addAll(project.getTeam().getTasks());
@@ -240,8 +138,7 @@ public class ManageProjectController extends BasicController {
                     }
                 }
             }
-            if(endDatePicker.getValue() != null){
-                System.out.println("Filtering with end date: ");
+            if(endDatePicker.getValue() != null){ // filter through due date
                 if(filteredList.size() == 0 && startDatePicker.getValue() == null){
                     System.out.println("\tNo checkbox filters were used, initializing filterList for end");
                     filteredList.addAll(project.getTeam().getTasks());
@@ -265,35 +162,19 @@ public class ManageProjectController extends BasicController {
                     }
                 }
             }
-            if(typeComboBox.getValue() != null && directionComboBox.getValue() != null){
-                System.out.println("Sorting with type: "+typeComboBox.getValue()+", and: "+directionComboBox.getValue());
-                switch(typeComboBox.getSelectionModel().getSelectedItem()){
-                    case "Total Comments":
-                        break;
-                    case "Alphabetical Task Title":
-                        break;
-                    case "Start Date":
-                        break;
-                }
-                switch(directionComboBox.getSelectionModel().getSelectedItem()){
-                    case "Ascending":
-                        break;
-                    case "Descending":
-                        break;
-                }
-            }
-            System.out.println("Displaying filtered list onto UI");
-            tasksObservableList = FXCollections.observableList(filteredList);
-            tasksTableView.setItems(tasksObservableList);
-        } else{ // no filter or sort options were made
-            System.out.println("No filter or sort options detected, displaying original list");
-            displayOriginalTasks();
-        }
 
+            // show official filtered list
+            displayTasks(filteredList);
+        } else{ // no filters exist, show original list of tasks
+            System.out.println("No filter or sort options detected, displaying original list");
+            displayTasks(project.getTeam().getTasks());
+        }
     }
-    public void handleClearButton(ActionEvent event){
+
+    public void handleClearButton(ActionEvent event){ // clear filter options
         System.out.println("Clear Tapped");
-        displayOriginalTasks();
+        displayTasks(project.getTeam().getTasks());
+
         boxesChecked.clear();
         statusToDoCheckBox.setSelected(false);
         statusInProgressCheckBox.setSelected(false);
@@ -307,18 +188,89 @@ public class ManageProjectController extends BasicController {
         startDatePicker.getEditor().clear();
         endDatePicker.setValue(null);
         endDatePicker.getEditor().clear();
-        typeComboBox.getSelectionModel().clearSelection();
-        typeComboBox.setPromptText("Type");
-        directionComboBox.getSelectionModel().clearSelection();
-        directionComboBox.setPromptText("Direction");
     }
-    public void handleLogoutButton(ActionEvent event){
+
+    public void handleRenameProjectButton(ActionEvent event){ // rename project
+        System.out.println("Rename Project Tapped");
+    }
+
+    public void handleRenameTeamButton(ActionEvent event){ // rename team
+        System.out.println("Rename Team Tapped");
+    }
+
+    public void handleMembersButton(ActionEvent event){ // add delete or edit members
+        System.out.println("Members Tapped");
+    }
+
+    public void handleLogoutButton(ActionEvent event){ // logout
         System.out.println("Logout Tapped");
         changeScreen("../view/login.fxml", event, new LoginController());
     }
-    public void handleHomeButton(ActionEvent event){
+
+    public void handleHomeButton(ActionEvent event){ // go to home page
         System.out.println("Home Tapped");
         BasicController controller = changeScreen("../view/Home.fxml", event, new HomeController());
         controller.start(project, member);
+    }
+
+    public void handleViewAnalyticsButton(ActionEvent event){ // go to analytics page
+        System.out.println("View Analytics Tapped");
+    }
+
+    private void displayTasks(List<Task> list){ // choose which list to display
+        tasksObservableList = FXCollections.observableList(list);
+        tasksTableView.setItems(tasksObservableList);
+    }
+
+    private void printCurrentState(){ // check date is passed correctly
+        System.out.println("Printing Current State of ManageProjectController: ");
+        System.out.println("\tCurrent Member: "+member.getName());
+        System.out.println("\tProject Name: "+project.getTitle());
+        System.out.println("\tTeam name: "+project.getTeam().getTitle());
+        for(Member m : project.getTeam().getMembers()){
+            System.out.println("\tMember: "+m.getName());
+        }
+        for(Task t : project.getTeam().getTasks()){
+            System.out.println("\tTask: "+t.toString());
+        }
+    }
+
+    private void refreshScreen(){ // update UI
+        this.projectText.setText(project.getTitle());
+        this.teamText.setText(project.getTeam().getTitle());
+    }
+
+    private boolean filterExists(){ // check if any filter option is checked or date filter exists
+        List<CheckBox> checkBoxes = new ArrayList<>();
+        checkBoxes.add(statusToDoCheckBox);
+        checkBoxes.add(statusInProgressCheckBox);
+        checkBoxes.add(statusHaltedCheckBox);
+        checkBoxes.add(statusDoneCheckBox);
+        checkBoxes.add(diffEasyCheckBox);
+        checkBoxes.add(diffMediumCheckBox);
+        checkBoxes.add(diffHardCheckBox);
+        checkBoxes.add(diffUnknownCheckBox);
+        List<DatePicker> datePickers = new ArrayList<>();
+        datePickers.add(startDatePicker);
+        datePickers.add(endDatePicker);
+        return checkBoxes.stream().anyMatch(CheckBox::isSelected) ||
+                datePickers.stream().anyMatch(s -> s.getValue()!=null);
+    }
+
+    private void addActionToCheckBox(CheckBox ch){ // give each checkbox a function
+        ch.setOnAction(i->updateFilter(ch));
+    }
+
+    private void updateFilter(CheckBox ch) { // update list of checked checkboxes
+        System.out.println("update filter method called");
+        if(ch.isSelected()) {
+            if(!boxesChecked.contains(ch)){
+                boxesChecked.add(ch);
+            }
+        } else {
+            if(boxesChecked.contains(ch)){
+                boxesChecked.remove(ch);
+            }
+        }
     }
 }
